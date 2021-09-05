403911c2a46ddf42033aa75ca206009f55c130df
Jack <u20201a963@upc.edu.pe>
2021-09-04 21:02:03 -0500

    add true table and  karnaugh map
    revision

--- src/k3v.py
+++ src/k3v.py
@@ -1,76 +1,52 @@
-posib = 8
-listing = [ ] 
-var = " "
-line1 = ()
-line2 = ()
+def tabla3():
+    print(" P  Q  R ")
+    print("----------")
+    print(" 0  0  0 | [1]")
+    print(" 0  0  1 | [2]")
+    print(" 0  1  0 | [3]")
+    print(" 0  1  1 | [4]")
+    print(" 1  0  0 | [5]")
+    print(" 1  0  1 | [6]")
+    print(" 1  1  0 | [7]")
+    print(" 1  1  1 | [8]")
+    posib =  8
+    listing = [ ]
+    var = " "
+    for i in range (0,7):
+      print("variables para [", i , "]");
+      listing.append(int(input(var)))
+    return listing
+ 
 
-for i in range(0, posib):
-    print("variables para [", i, "]"); 
-    listing.append(int(input(var))) 
+#Para todos los ceros
+def mapaKarnaugh3 (listing):
+    print("P \\ QR   00   01   11   10")
+    print("         |-------------------")
+    print("    0    |  ", listing[0] ,"  |  ",listing[2],"  |  ",listing[6],"  |  ",listing[4]," |")
+    print("         |-------------------")
+    print("    1    |  ", listing[1] ,"  |  ",listing[3],"  |  ",listing[7],"  |  ",listing[5]," |")
+  
+    print("P \\ QR     00      01      11      10")
+    print("         ---------------------------------")
+    print("    0    |  [1]  |  [3]  |  [7]  |  [5]  |")
+    print("         ---------------------------------")
+    print("    1    |  [2]  |  [4]  |  [8]  |  [6]  |") 
+    
+    line1 = ()
+    line2 = ()
+    line1 = (listing[0],listing[2],listing[6],listing[4])
+    line2 = (listing[1],listing[3],listing[7],listing[5])
 
-line1 = (listing[0], listing[1])
-line2 = (listing[2], listing[3])
+    if line1 == (0, 0, 0, 0) and line2 == (0, 0, 0, 0):
+        print("No hay grupos")
+        print("Funcion simplificada, S( P , Q , R )  = 0")
 
+    elif line1 == (1, 1, 1, 1) and line2 == (1, 1, 1, 1):
+        print("Grupo de 8 = [1], [2], [3], [4], [5], [6], [7], [8]")
+        print("Funcion simplificada, S( P , Q , R )  = 1")
 
-# Para todos ceros 
-if line1 == (0, 0, 0, 0) and line2 == (0, 0, 0, 0) and \
-        line3 == (0, 0, 0, 0) and line4 == (0, 0, 0, 0):
-    print("No hay grupos")
-    print("Funcion simplificada, S(P, Q) = 0")
-
-# Para todos unos
-elif line1 == (1, 1) and line2 == (1, 1):
-    print("Grupo de 4 = { [1], [2], [3], [4] }")
-    print("Funcion simplificada, S(P, Q) = 1")
-# Para tres unos
-elif line1 == (0, 1) and line2 == (1, 1):
-    print("Grupo de 2 = { [3], [4] }")
-    print("Grupo de 2 = { [2], [4] }")
-    print("Funcion simplificada, S(P, Q) = Q + P")
-elif line1 == (1, 0) and line2 == (1, 1):
-    print("Grupo de 2 = { [1], [3] }")
-    print("Grupo de 2 = { [3], [4] }")
-    print("Funcion simplificada, S(P, Q) = Q' + P")
-elif line1 == (1, 1) and line2 == (0, 1):
-    print("Grupo de 2 = { [1], [2] }")
-    print("Grupo de 2 = { [2], [4] }")
-    print("Funcion simplificada, S(P, Q) = P' + Q")
-elif line1 == (1, 1) and line2 == (1, 0):
-    print("Grupo de 2 = { [1], [2] }")
-    print("Grupo de 2 = { [1], [3] }")
-    print("Funcion simplificada, S(P, Q) = P' + Q'")
-# Para dos unos - adj 
-elif line1 == (1, 1) and line2 == (0, 0):
-    print("Grupo de 2 = { [1], [2] }")
-    print("Funcion simplificada, S(P, Q) = P'")
-elif line1 == (0, 0) and line2 == (1, 1):
-    print("Grupo de 2 = { [3], [4] }")
-    print("Funcion simplificada, S(P, Q) = P")
-elif line1 == (1, 0) and line2 == (1, 0):
-    print("Grupo de 2 = { [1], [3] }")
-    print("Funcion simplificada, S(P, Q) = Q'")
-elif line1 == (0, 1) and line2 == (0, 1):
-    print("Grupo de 2 = { [2], [4] }")
-    print("Funcion simplificada, S(P, Q) = Q")
-# Para dos unos - sep 
-elif line1 == (1, 0) and line2 == (0, 1):
-    print("Grupo de 1 = { [1] }")
-    print("Grupo de 1 = { [4] }")
-    print("Funcion simplificada, S(P, Q) = P'Q' + PQ")
-elif line1 == (0, 1) and line2 == (0, 1):
-    print("Grupo de 1 = { [3] }")
-    print("Grupo de 1 = { [2] }")
-    print("Funcion simplificada, S(P, Q) = P'Q + PQ'")
-# Para unos
-elif line1 == (1, 0) and line2 == (0, 0):
-    print("Grupo de 1 = { [1] }")
-    print("Funcion simplificada, S(P, Q) = P'Q'")
-elif line1 == (0, 1) and line2 == (0, 0):
-    print("Grupo de 1 = { [2] }")
-    print("Funcion simplificada, S(P, Q) = P'Q")
-elif line1 == (0, 0) and line2 == (1, 0):
-    print("Grupo de 1 = { [3] }")
-    print("Funcion simplificada, S(P, Q) = P'Q")
-elif line1 == (0, 0) and line2 == (0, 1):
-    print("Grupo de 1 = { [4] }")
-    print("Funcion simplificada, S(P, Q) = PQ")
+    elif line1 == (1,1,1,0) and line2 == (1,1,1,1):
+        print("Grupo de 4 = [1],[2],[3],[4]")
+        print("Grupo de 4 = [3],[4],[7],[8]") 
+        print("Grupo de 4 = [2],[4],[8],[6]")
+        print("Funcion simplificada, f(P, Q, R) = Q' + R + P ")# Para todos unos
